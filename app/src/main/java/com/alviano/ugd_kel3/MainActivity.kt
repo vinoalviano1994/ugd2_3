aedc422a48ab3b155bfe88681e5ee3406c826a9f
Dyonisius Alviano M E <vinoalviano1994@gmail.com>
2022-09-09 15:17:17 +0700

    mainactivity .kt = tambah menu dikit untuk login (masih proses, jangan di edit dulu) activity main . xml = masih design (jgn diubah dulu)

--- app/src/main/java/com/alviano/ugd_kel3/MainActivity.kt
+++ app/src/main/java/com/alviano/ugd_kel3/MainActivity.kt
@@ -2,6 +2,9 @@ package com.alviano.ugd_kel3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
@@ -20,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         inputUssername = findViewById(R.id.etUserrname)
         inputPassword = findViewById(R.id.etPassword)
         mainLayout = findViewById(R.id.mainLayout)
-        val btnClear: Button
+        val btnClear: Button = findViewById(R.id.btLogin)
+        val btnRegister: TextView = findViewById(R.id.tvRegister)
+        val btnLogin: Button = findViewById(R.id.btLogin)
+
+        btnClear.setOnClickListener{
+            inputUssername.getEditText()?.setText("")
+        }
+
+
     }
 }
